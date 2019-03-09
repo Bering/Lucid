a0085ed00656638ac3965329baf895f38a2adf68
Bering <bering@ringlogic.com>
2019-03-09 17:25:14 -0500

    BOOM!

--- dao/project.py
+++ dao/project.py
@@ -18,45 +18,56 @@ class ProjectDAO:
 					"name" : "LUCID",
 					"lists" : [ "TODO", "Doing", "Done" ],
 					"cards" : {
-						"0" : {
-							"id" : 0,
-							"title" : "304 Not Modified",
-							"description" : "",
-							"labels" : []
+						"0" : { # list index 0
+							"0" : { # card id 0
+								"id" : 0,
+								"list_index" : 0,
+								"position" : 0,
+								"title" : "304 Not Modified",
+								"description" : "",
+								"labels" : []
+							},
+							"2" : {
+								"id" : 2,
+								"list_index" : 0,
+								"position" : 2,
+								"title" : "SQLite",
+								"description" : "",
+								"labels" : []
+							}
 						},
-						"1" : {
-							"id" : 1,
-							"title" : "Project interface",
-							"description" : """- [x] project list
-- [ ] Username
-- [ ] project renaming
-- [ ] project switcher
-- [ ] display lists
-- [ ] renaming lists
-- [ ] adding lists
-- [ ] removing lists
-- [ ] adding cards
-- [ ] card title
-- [ ] card description
-- [ ] drag and dropping cards between lists
-- [ ] drag and dropping card in same list
-- [ ] project add
-- [ ] project delete
-- [ ] user add
-- [ ] user edit
-- [ ] user delete
-- [ ] card labels
-- [ ] card markdown
-- [ ] card checklists
-""",
-							"labels" : []
+						"1" : { # list index 1
+							"1" : { # card id 1
+								"id" : 1,
+								"list_index" : 0,
+								"position" : 1,
+								"title" : "Project interface",
+								"description" : """- [x] project list
+	- [ ] Username
+	- [ ] project renaming
+	- [ ] project switcher
+	- [ ] display lists
+	- [ ] renaming lists
+	- [ ] adding lists
+	- [ ] removing lists
+	- [ ] adding cards
+	- [ ] card title
+	- [ ] card description
+	- [ ] drag and dropping cards between lists
+	- [ ] drag and dropping card in same list
+	- [ ] project add
+	- [ ] project delete
+	- [ ] user add
+	- [ ] user edit
+	- [ ] user delete
+	- [ ] card labels
+	- [ ] card markdown
+	- [ ] card checklists
+	""",
+								"labels" : []
+							}
 						},
-						"2" : {
-							"id" : 2,
-							"title" : "SQLite",
-							"description" : "",
-							"labels" : []
-						},
+						"2" : {}
 					}
 				},
 				"Master-of-Stars" : {
