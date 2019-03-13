f71e0bac5a7a84829bfb09a469f85603d46060a5
Bering <bering@ringlogic.com>
2019-03-13 13:08:07 -0400

    Accept command line parameters for hostname and port

--- __main__.py
+++ __main__.py
@@ -1,10 +1,43 @@
+import sys
 import time
-import config
+import getopt
+from config import Config
 from http.server import HTTPServer
 from server import LucidServer
 
-httpd = HTTPServer((config.hostname, config.port), LucidServer)
-print(time.asctime(), ": LUCID server started on", config.hostname + ":" + str(config.port))
+config = Config()
+user_config = config.load()
+hostname = user_config["hostname"]
+port = user_config["port"]
+
+usage = 'LUCID -p <port> <hostname> or <hostname:port>'
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"hp:",["port="])
+except getopt.GetoptError:
+	print (usage)
+	sys.exit(2);
+for opt, arg in opts:
+	if opt == '-h':
+		print (usage)
+		sys.exit();
+	elif opt in ("-p", "--port"):
+		port = arg
+if len(args):
+	hostname = args[0]
+
+if "-p" not in opts and "--port" not in opts:
+	hostname_parts = hostname.split(":")
+	if len(hostname_parts) == 2:
+		hostname = hostname_parts[0]
+		port = hostname_parts[1]
+
+if len(sys.argv) > 1:
+	print("Config saved: hostname=" + hostname + ", port=" + str(port) + " in file: ", config.config_file)
+	port = int(port)
+	config.save(hostname, port)
+
+httpd = HTTPServer((hostname, port), LucidServer)
+print(time.asctime(), ": LUCID server started on", hostname + ":" + str(port))
 
 try:
 	httpd.serve_forever()
--- config.py
+++ config.py
@@ -2,24 +2,32 @@ import os
 import json
 from appdirs import AppDirs
 
-dirs = AppDirs("LUCID", "RingLogic")
-if not os.path.exists(dirs.user_config_dir):
-	os.makedirs(dirs.user_config_dir)
+class Config:
+	def __init__(self):
+		dirs = AppDirs("LUCID", "RingLogic")
+		if not os.path.exists(dirs.user_config_dir):
+			os.makedirs(dirs.user_config_dir)
 
-config_file = os.path.join(dirs.user_config_dir, "config.json")
-if not os.path.exists(config_file):
-	fh = open(config_file, "w+")
-	config = {
-		"hostname" : "localhost",
-		"port" : 8888
-	}
-	fh.write(json.dumps(config, indent=4))
-	fh.close()
+		self.config_file = os.path.join(dirs.user_config_dir, "config.json")
+		if not os.path.exists(self.config_file):
+			self.save("localhost", 8888)
 
-fh = open(config_file, "r")
-config = json.loads(fh.read())
-fh.close()
+	def load(self):
+		fh = open(self.config_file, "r")
+		config = json.loads(fh.read())
+		fh.close()
+		return config
 
+	def save(self, h, p):
+		config = {}
+		config["hostname"] = h
+		config["port"] = int(p)
+		fh = open(self.config_file, "w+")
+		fh.write(json.dumps(config, indent=4))
+		fh.close()
+		return config
+
+# path is not part of config that is dumped to json file
+# TODO: Not sure how this will behave when I use PyInstaller
+# TODO: Test again to make sure it works when invoked in another project's folder
 path = os.path.dirname(os.path.abspath(__file__))
-hostname = config["hostname"]
-port = config["port"]
