3d2fa7f004a65a4b059bfbf26b621edd5200d198
Philippe Gouin <bering@ringlogic.com>
2019-05-28 21:32:01 -0400

    log file in Linux

--- lucid.sh
+++ lucid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/bering/Documents/Lucid
-/usr/bin/python3 /home/bering/Documents/Lucid/__main__.py
+/usr/bin/python3 /home/bering/Documents/Lucid/__main__.py >/home/bering/.config/LUCID/lucid.log &2>1
